39896536cab35dd94bcd40d39984ba29b38d47ae
Aditya Trivedi <adityatrivedi@Adityas-MacBook-Air.local>
2022-03-13 02:30:38 +0530

    Initial Commit

---
+++ src/BlogDetails.js
@@ -0,0 +1,33 @@
+import { useNavigate, useParams } from "react-router-dom";
+import  useFetch  from "./useFetch";
+
+const BlogDetails = () => {
+    const { id } = useParams();
+    const { data : blog, isPending , error } = useFetch('http://localhost:8000/blogs/' + id);
+    const navigate = useNavigate();
+
+    const handleDelete = () =>{
+        fetch('http://localhost:8000/blogs/' + id , {
+            method : 'DELETE'
+        }).then(() =>{
+            navigate("/")
+        })
+    }
+
+    return ( 
+        <div className="blog-details">
+            { isPending && <div>Loading...</div> }
+            { error && <div>{ error }</div> }
+            { blog && (
+                <article>
+                    <h2>{ blog.title }</h2>
+                    <p>Written By : { blog.author }</p>
+                    <div>{ blog.body }</div>
+                    <button onClick={ handleDelete }>Delete</button>
+                </article>
+            )}
+        </div>
+    );
+}
+ 
+export default BlogDetails;
